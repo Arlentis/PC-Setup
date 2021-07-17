d25e76441e62ae5d889fd7209e2b8cb4f7b519a4
Arlentis <87531272+Arlentis@users.noreply.github.com>
2021-07-17 14:48:45 +0100

    Delete 6. Office&Eset.ps1

--- 6. Office&Eset.ps1
+++
@@ -1,7 +0,0 @@
-﻿New-PSDrive -Name "X" -PSProvider FileSystem -Root "\\fons\sharedx" -Credential HLX\CallumL -Persist
-
-$OfficeSetup = "\\fons\sharedx\healix it\callum\installs\Microsoft Office\Office 2013 Standard 32bit\setup.exe"
-$ESETSetup = '\\fons\sharedx\healix it\temp\ESMC_Installer_x64_en_US_DHURLTEST.exe'
-
-Start-Process -FilePath $OfficeSetup -Verbose
-Start-Process -FilePath $ESETSetup -Verbose
