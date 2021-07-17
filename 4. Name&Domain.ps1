25a175dc743da02aeffabd9bda1b821c76c13b77
Arlentis <87531272+Arlentis@users.noreply.github.com>
2021-07-17 14:48:44 +0100

    Delete 4. Name&Domain.ps1

--- 4. Name&Domain.ps1
+++
@@ -1,8 +0,0 @@
-﻿[System.Reflection.ASsembly]::LoadWithPartialName("Microsoft.VisualBasic")
-
-$ComputerName1 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Computer Name","PC Name","HLX-")
-$ComputerName = $ComputerName1
-
-Rename-Computer -ComputerName $env:COMPUTERNAME -NewName $ComputerName -LocalCredential "$env:COMPUTERNAME\$env:USERNAME" -Force
-
-Add-Computer -DomainName hlx.int -Credential HLX\CLAdmin -Restart -Force
