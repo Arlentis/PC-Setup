2e8f0cf6ad3b5a3fe32e7c0b097a8c71c36b4181
Arlentis <87531272+Arlentis@users.noreply.github.com>
2021-12-23 11:47:56 +0000

    Update 4. OfficeESETBitlocker&Bits.ps1
    Added in LanSweeper Agent auto install

--- 4. OfficeESETBitlocker&Bits.ps1
+++ 4. OfficeESETBitlocker&Bits.ps1
@@ -108,20 +108,33 @@ Remove-Item $MimecastWebSecurity -Force -ErrorAction SilentlyContinue
 Remove-Item $MimecastCustomerKeyPath1
 Remove-Item $MimecastCustomerKeyPath2
 
+### INSTALL LANSWEEPER AGENT
+
+$LSAgent ="https://github.com/Arlentis/Work-Healix/raw/main/LsAgent-windows.msi"
+$LSAgentPath = "$env:USERPROFILE\Downloads\LsAgent-windows.msi"
+Invoke-WebRequest $LSAgent -OutFile $LSAgentPath -Verbose
+Start-Sleep -Seconds 5
+
+Start-Process $LSAgentPath
+
+Start-Sleep -Seconds 20
+Remove-Item $LSAgentPath -Force -ErrorAction SilentlyContinue
+
 ### CHANGE COMPUTER LOCATION AND DESCRIPTION
 
-[System.Reflection.ASsembly]::LoadWithPartialName("Microsoft.VisualBasic")
+#[System.Reflection.ASsembly]::LoadWithPartialName("Microsoft.VisualBasic")
 
-$ComputerLocation1 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Computer Location","PC Location","Who's using this computer?")
-$ComputerLocation = $ComputerLocation1
+#$ComputerLocation1 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Computer Location","PC Location","Who's using this computer?")
+#$ComputerLocation = $ComputerLocation1
 
-$ComputerDescription1 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Computer Description","PC Description","Asset Sticker? What's this being used for? Service Desk Number?")
-$ComputerDescription = $ComputerDescription1
+#$ComputerDescription1 = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Computer Description","PC Description","Asset Sticker? What's this being used for? Service Desk Number?")
+#$ComputerDescription = $ComputerDescription1
 
-$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://iris.hlx.int/powershell/ -Credential HLX\CLAdmin -Authentication Kerberos
-Import-PSSession $session -AllowClobber -Verbose
+#$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://iris.hlx.int/powershell/ -Credential HLX\CLAdmin -Authentication Kerberos
+#Import-PSSession $session -AllowClobber -Verbose
 
-Set-ADComputer -Identity $env:COMPUTERNAME -Location $ComputerLocation -Verbose
-Set-ADComputer -Identity $env:COMPUTERNAME -Description $ComputerDescription -Verbose
+#Set-ADComputer -Identity $env:COMPUTERNAME -Location $ComputerLocation -Verbose
+#Set-ADComputer -Identity $env:COMPUTERNAME -Description $ComputerDescription -Verbose
 
-Remove-PSSession $session 
+#Remove-PSSession $session
+
